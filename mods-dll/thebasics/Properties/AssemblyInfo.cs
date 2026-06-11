b39877b044dcd8d62dce9d190fcd99c58784d378
BASICBIT <basic.bit.1001@gmail.com>
2026-06-11 19:32:48 -0400

    Bump The BASICs to 5.8.0

--- mods-dll/thebasics/Properties/AssemblyInfo.cs
+++ mods-dll/thebasics/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using Vintagestory.API.Common;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 [assembly: ModInfo("The BASICs", "thebasics",
-    Version = "5.6.0",
+    Version = "5.8.0",
     Description = "Adds an RP proximity chat system, with configurable talking ranges, nicknames, automatic message formatting, and more! Also adds a server save notification.",
     Authors = new[] { "BASIC" })]
 
